ee970ba7ff9aea9461758961b601864905c17295
John <jjkiely@gmail.com>
2017-10-11 22:28:54 +0100

    added typescript syntax

--- lib/bloom_factory.ts
+++ lib/bloom_factory.ts
@@ -1,15 +1,21 @@
 import * as farmhash from 'farmhash';
 
-class Bloom {
-    constructor(expected_items=100, false_positive=0.01) {
+export class Bloom {
+    bits_per_item: number;
+    len: number;
+    hashes: number;
+    bits: number[];
+    farmhash: any;
+
+    constructor(expected_items: number = 100, false_positive: number = 0.01) {
         this.bits_per_item = this._bits_per_item(false_positive)
         this.len = expected_items * this.bits_per_item
         this.hashes = Math.ceil(0.7 * this.bits_per_item)
-        this.bits = Array(this.len).fill(0)
+        this.bits = Array(this.len).map(Number.prototype.valueOf, 0)
         this.farmhash = farmhash
     }
 
-    insert(item) {
+    public insert(item: any) {
         if (Array.isArray(item)) {
             item.forEach(i => {
                 this.insert(i)
@@ -22,7 +28,7 @@ class Bloom {
         }
     }
 
-    has(item) {
+    public has(item: any): boolean {
         const hl = this.hash(item)
         hl.forEach(h => {
             if (this.bits[h] === 0) {
@@ -32,7 +38,7 @@ class Bloom {
         return true
     }
 
-    does_not_have(item) {
+    public does_not_have(item: any): boolean {
         const hl = this.hash(item)
         hl.forEach(h => {
             if (this.bits[h] === 1) {
@@ -42,7 +48,7 @@ class Bloom {
         return true
     }
 
-    hash(item) {
+    private hash(item: any): number[] {
         if (typeof item === 'string') {
             return this._hashes(item)
         } else if (typeof item === 'number') {
@@ -50,7 +56,7 @@ class Bloom {
         }
     }
 
-    _hashes(item) {
+    private _hashes(item: string): number[] {
         const h1 = this.farmhash.hash32WithSeed(item, 1)
         const h2 = this.farmhash.hash32WithSeed(item, 2)
         let hashes = [(h1 % this.len), (h2 % this.len)]
@@ -60,14 +66,12 @@ class Bloom {
         return hashes
     }
 
-
-    _combine(hash1, hash2) {
+    private _combine(hash1: number, hash2: number): number {
         return ((hash1 + hash2) % this.len)
     }
 
-    _bits_per_item(false_pos) {
+    private _bits_per_item(false_pos: number): number {
         return Math.ceil(Math.log(100/false_pos)/(Math.log(2)**2))
     }
 }
 
-module.exports = Bloom;
